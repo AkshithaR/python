d149d1af40704b6e7c6ad546c963ff0a0b829ecb
Akshitha Raghavendran <30437379+AkshithaR@users.noreply.github.com>
2021-12-29 04:33:29 +0000

    added string formating

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+#string formating 
 age =30
-print(age)
+print(f"you are {age}")
+####
+name="Akshi"
+greeting="how are you {name}"
+print(greeting.format(name=name))
+#or
+greeting="how are you {}"
+print(greeting.format(name))
+
